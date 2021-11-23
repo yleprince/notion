ac657f72873585a04a7472fb58a35257a96139ff
YLeprince <32041754+yleprince@users.noreply.github.com>
2021-11-23 14:39:49 +0100

    Adding notion client 👨🏻‍🎨  (#4)
    * Adding notion python client
    * add .vscode to gitignore
    * adding test

---
+++ notion/client.py
@@ -0,0 +1,30 @@
+from os import getenv
+
+import requests
+from dotenv import load_dotenv
+from requests.models import Response
+
+from notion.types import Page
+
+load_dotenv()
+
+
+class NotionClient:
+    def __init__(self, bearer_token: str = None):
+        self.bearer_token = bearer_token or getenv("NOTION_TOKEN")
+        self.headers = {
+            "Authorization": f"Bearer {self.bearer_token}",
+            "Notion-Version": "2021-08-16",
+        }
+        self.api_root: str = "https://api.notion.com/v1"
+
+    def _get_page(self, page_uuid: str) -> Response:
+        page_route = f"{self.api_root}/pages/{page_uuid}"
+        return requests.get(page_route, headers=self.headers)
+
+    def get_page(self, page_uuid: str) -> Page:
+        r = self._get_page(page_uuid=page_uuid).json()
+        return Page(**r)
+
+    def __str__(self) -> str:
+        return self.bearer_token
